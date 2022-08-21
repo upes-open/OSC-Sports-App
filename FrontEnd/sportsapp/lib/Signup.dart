7ac8a0dae33d46f459a77909731c4e7c6975301f
Kapoor-0905 <500097111@stu.upes.ac.in>
2022-08-21 22:34:01 +0530

    Login/Signup and landing page added

---
+++ FrontEnd/sportsapp/lib/Signup.dart
@@ -0,0 +1,289 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'package:sportsapp/main.dart';
+import 'package:flutter/services.dart';
+import 'package:country_picker/country_picker.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:country_list_pick/country_list_pick.dart';
+import 'package:flutter/widgets.dart';
+import 'package:sportsapp/landing.dart';
+
+class SignupPage extends StatelessWidget {
+  @override
+  bool agree = false;
+  bool isButtonActive = true;
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        centerTitle: true,
+        title: Text(
+          "Sports App",
+          style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
+        ),
+        backgroundColor: Colors.white,
+        leading: /*IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
+            )),*/
+            Container(
+          child: Image.asset(
+            'assets/images/open.png',
+            fit: BoxFit.fitWidth,
+            width: 100,
+            height: 100,
+          ),
+          margin: const EdgeInsets.only(left: 15.0, right: 0),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Sign up",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          height: 30,
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            child: TextField(
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Full Name',
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Email',
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          IntlPhoneField(
+                            decoration: InputDecoration(
+                              //decoration for Input Field
+                              labelText: 'Phone Number',
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(),
+                              ),
+                            ),
+                            initialCountryCode:
+                                'IN', //default contry code, NP for Nepal
+                            onChanged: (phone) {
+                              //when phone number country code is changed
+                              print(phone.completeNumber); //get complete number
+                              print(phone.countryCode); // get country code only
+                              print(phone.number); // only phone number
+                            },
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Country',
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextField(
+                            inputFormatters: [
+                              new FilteringTextInputFormatter.allow(
+                                  RegExp("[0-9.]")),
+                              LengthLimitingTextInputFormatter(2),
+                            ],
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Age',
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Sport',
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                    /*makeInput(label: "Email"),
+                          makeInput(label: "Password", obsureText: true),
+                          makeInput(label: "Confirm Pasword", obsureText: true)*/
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      /*child: Container(
+                        padding: EdgeInsets.only(top: 3, left: 3),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            border: Border(
+                                bottom: BorderSide(color: Colors.black),
+                                top: BorderSide(color: Colors.black),
+                                right: BorderSide(color: Colors.black),
+                                left: BorderSide(color: Colors.black))),*/
+                      /*child: Container(
+                            height: 50,
+                            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.green,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(40)),
+                                ),
+                                child: const Text(
+                                  'Sign up',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                                onPressed: () {})),*/
+                      child: Container(
+                        height: 50,
+                        width: double.infinity,
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.blue,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40)),
+                          ),
+                          child: const Text(
+                            'Sign up',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Landing()));
+                          },
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        const Text('Already have an account?'),
+                        TextButton(
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MyApp(),
+                              ),
+                            );
+                            //signup screen
+                          },
+                        )
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*Widget makeInput({label, obsureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        label,
+        style: TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      SizedBox(
+        height: 5,
+      ),
+      TextField(
+        obscureText: obsureText,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+          border:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+        ),
+      ),
+      SizedBox(
+        height: 30,
+      )
+    ],
+  );
+}
+*/
+
+/*Checkbox(
+                            focusColor: Colors.lightBlue,
+                            activeColor: Colors.green,
+                            value: agree,
+                            onChanged: (value) {
+                              setState(() {
+                                agree = value;
+                              });
+                            },
+                          ), */
---
+++ FrontEnd/sportsapp/lib/login.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:sportsapp/Login.dart';
+import 'dart:ui';
+import 'package:intl/intl.dart';
+import 'dart:async';
+import 'package:sportsapp/Signup.dart';
+
+void main() => runApp(const MyApp());
+
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Sports App';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Text(
+            "Sports App",
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          leading: Container(
+            child: Image.asset(
+              'assets/images/open.png',
+              fit: BoxFit.fitWidth,
+              width: 100,
+              height: 100,
+            ),
+            margin: const EdgeInsets.only(left: 15.0, right: 0),
+          ),
+          elevation: 0,
+        ),
+        backgroundColor: Colors.white,
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(fontSize: 20),
+                )),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: () {
+                //forgot password screen
+              },
+              child: const Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40)),
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  onPressed: () {
+                    print(nameController.text);
+                    print(passwordController.text);
+                  },
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: <Widget>[
+                const Text('Does not have account?'),
+                TextButton(
+                  child: const Text(
+                    'Sign in',
+                    style: TextStyle(fontSize: 18, color: Colors.blue),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SignupPage(),
+                      ),
+                    );
+                    //signup screen
+                  },
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+          ],
+        ));
+  }
+}
+
+/// Signup screen
