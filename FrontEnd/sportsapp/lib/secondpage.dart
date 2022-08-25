047d66eca1fd1e23a06105c98a965e9d25f2a384
Charu-2718 <charu.15421@gmail.com>
2022-08-25 14:28:04 +0530

    home page added and linked

---
+++ FrontEnd/sportsapp/lib/secondpage.dart
@@ -0,0 +1,68 @@
+import 'dart:ui';
+import 'package:sportsapp/landing.dart';
+import 'package:country_list_pick/country_list_pick.dart';
+import 'package:country_picker/country_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:sportsapp/Signup.dart';
+import 'package:sportsapp/main.dart';
+import 'package:sportsapp/login.dart';
+
+class secondpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome To The World of Sports !'),
+          centerTitle: true,
+          leading: Container(child: Image.asset('assets/images/open.png')),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Image.asset(
+                'assets/images/second.jpg',
+                width: 600,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 40),
+              child: ElevatedButton(
+                child: const Text('SignUp'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SignupPage()));
+                },
+                style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.blue,
+                    elevation: 50,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 80),
+              child: ElevatedButton(
+                child: const Text('LogIn'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyApp()));
+                },
+                style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.blue,
+                    elevation: 50,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+              ),
+            ),
+          ],
+        )));
+  }
+}
